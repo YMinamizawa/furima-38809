317e1dff47eb7f0da61cb1dc83b845e21310b7eb
YMinamizawa <yoshiki19871984@gmail.com>
2023-01-13 20:30:07 +0900

    新規登録、ログイン、ログアウト機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+         
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々]+\z/} do
+    validates :family_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/} do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+
+  validates :nickname, presence: true
+  validates :birth_day, presence: true
+    end
