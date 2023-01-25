a27702c73c5a6ee128c46d6d327934007b2ea6fb
YMinamizawa <yoshiki19871984@gmail.com>
2023-01-25 10:52:44 +0900

    購入、保存までの実装

---
+++ app/models/order_form.rb
@@ -0,0 +1,24 @@
+class OrderForm
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchase_record #:token
+    
+  with_options presence: true do
+    # orderモデルのバリデーション
+    validates :user_id
+    validates :item_id
+    # paymentモデルのバリデーション
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+    # トークンのバリデーション
+    #validates :token
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    # ストロングパラメーターでデータが運ばれ、それらが保存のタイミングで「order_id」が生成され、保存される。
+    PaymentMethod.create(order_id: order.id, postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number)
+  end
+end
