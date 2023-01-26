2dfd5d618f82896fbec31e8fddf4bcb94a423657
YMinamizawa <yoshiki19871984@gmail.com>
2023-01-26 11:54:17 +0900

    指摘箇所の修正

--- app/models/item_condition.rb
+++ app/models/item_condition.rb
@@ -5,7 +5,7 @@ class ItemCondition < ActiveHash::Base
     { id: 2, item_name: '未使用品に近い' },
     { id: 3, item_name: '目立った傷や汚れなし' },
     { id: 4, item_name: 'やや傷や汚れあり' },
-    { id: 5, itme_name: '傷や汚れあり' },
+    { id: 5, item_name: '傷や汚れあり' },
     { id: 6, item_name: '全体的に状態が悪い' }
   ]
 
--- app/models/payment_method.rb
+++ app/models/payment_method.rb
@@ -2,7 +2,7 @@ class PaymentMethod < ActiveHash::Base
   self.data = [
     { id: 0, item_name: '---' },
     { id: 1, item_name: '着払い（購入者負担）' },
-    { id: 2, item_name: '送料払い（出品者負担）' }
+    { id: 2, item_name: '送料込み(出品者負担)' }
   ]
 
   include ActiveHash::Associations
